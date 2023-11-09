b2b32d38b5f7a24def0efbc4a4cb256e6348e41d
JAMES-BONT-007 <90338385+JAMES-BONT-007@users.noreply.github.com>
2023-11-09 11:31:11 +0100

    Update rejestracja.php

--- rejestracja.php
+++ rejestracja.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,24 +9,17 @@
 <body>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nazwa = $_POST["nazwa"];
     $haslo = sha1($_POST["haslo"]); // Haszowanie hasła za pomocą SHA-1
 
     $conn = new mysqli("127.0.0.1", "root", "", "tictactoeDB");
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
     $q = "INSERT INTO users (nazwa, haslo) VALUES ('$nazwa', '$haslo')";
 
     if ($conn->query($q) === TRUE) {
-        // Użytkownik został zarejestrowany pomyślnie
     }
 
     $conn->close();
-}
 
 header("refresh:3;url=index.php"); // Przekierowanie do index.php po 3 sekundach
 ?>
