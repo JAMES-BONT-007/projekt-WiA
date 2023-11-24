81f0d4f426c06595590e83814499c85f3f0ed4b9
JAMES-BONT-007 <90338385+JAMES-BONT-007@users.noreply.github.com>
2023-11-24 23:02:14 +0100

    Update tictactoedb.sql

--- tictactoedb.sql
+++ tictactoedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 12 Lis 2023, 16:23
+-- Czas generowania: 23 Lis 2023, 13:19
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
@@ -24,18 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `log`
+--
+
+CREATE TABLE `log` (
+  `id_log` int(7) NOT NULL,
+  `opis operacji` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `uzytkownicy`
 --
 
 CREATE TABLE `uzytkownicy` (
   `id` int(6) NOT NULL,
   `nazwa` varchar(16) NOT NULL,
-  `haslo` varchar(55) NOT NULL
+  `haslo` varchar(40) NOT NULL,
+  `ranking` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id_log`);
 
 --
 -- Indeksy dla tabeli `uzytkownicy`
@@ -48,10 +66,16 @@ ALTER TABLE `uzytkownicy`
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `log`
+--
+ALTER TABLE `log`
+  MODIFY `id_log` int(7) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT dla tabeli `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
