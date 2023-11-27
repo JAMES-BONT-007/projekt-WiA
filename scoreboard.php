a9fddf8c07ee59fcac4706c217c8f5974363db61
JAMES-BONT-007 <90338385+JAMES-BONT-007@users.noreply.github.com>
2023-11-27 22:18:55 +0100

    gsa

--- scoreboard.php
+++ scoreboard.php
@@ -1,17 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="stylmenu.css">
-    <title></title>
+<html>
 
-    <style>
-    body{
+<head>
+    <meta charset="UTF-8">
+    <title>KS THE GAME</title>
+    <link rel="stylesheet" href="stylmenu.css">
+    <link rel="icon" type="image/x-icon" href="ave.ico">
+
+    <style> 
+    h1{
         color: white;
-    }    
+    }
+
+    table{
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    table, tr, td{
+
+        background-color: #33a6b8;
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+
+    tr:nth-child(1) > td{
+        background-color: #34eb46;
+
+    }
+
+    tr:nth-child(2) > td{
+        background-color: #fcb434;
+
+    }
+
+    tr:nth-child(3) > td{
+        background-color: silver;
+
+    }
+
+    tr:nth-child(4) > td{
+        background-color: #a34b2a;
+
+    }
+
     </style>
+
+
+
 </head>
+
 <body>
-    <div id="center">
+
+    <div id="naglowek1">
+        <?php
+
+        if (isset($_COOKIE["konto"])) {
+            $zalogowano = True;
+            $login = $_COOKIE["konto"];
+
+            echo "Zalogowano jako " . $login;
+        }
+
+        ?>
+
+
+    </div>
+    <div id="naglowek2">
+
+        <img id="logo" src="logo.png" alt="logo_gry" />
+
+    </div>
+    <div id="naglowek3">
+
+        <img src="mute-black.png" id="icon">
+
+    </div>
+    <div id="main">
+
     <h1>Oto ranking naszych graczy:</h1>
     
     <?php
@@ -33,9 +98,42 @@
             echo "<p>Nasza baza jeszcze nie ma żadnych JESZCZE użytkówników.</p>
             <p>Możesz być pierwszy!</p>";
 
-        }  
-
+        }
+        
     ?>
+
+
     </div>
+
+
+    <div id="stopka1">
+
+        <h5>&copy; MASNY TUTEL, WSZELKIE PRAWA ZASTRZEŻONE</h5>
+
+    </div>
+    <div id="stopka2">
+
+
+        <img id="firma" src="tutelmaciek.png" alt="logo_studia" />
+    </div>
+    <audio id="audio" loop>
+        <source src="main.mp3" type="audio/mp3">
+    </audio>
+    <script>
+        var audio = document.getElementById("audio");
+        var icon = document.getElementById("icon");
+
+        icon.onclick = function() {
+            if (audio.paused) {
+                audio.volume = 0.5;
+                audio.play();
+                icon.src = "icon-black.png";
+            } else {
+                audio.pause();
+                icon.src = "mute-black.png";
+            }
+        }
+    </script>
 </body>
+
 </html>
